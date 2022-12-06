b405a9b63e6db876104f1f920463a1a81bf8936b
maanas <maanas2605@gmail.com>
2022-12-06 17:57:03 +0530

    Added show funcitonality to view the articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
    root "pages#home"
    get 'about', to:'pages#about'
+
+   #there will be many routes associated with a databse item inorder to perform crud operatios..hence rails provides us with a useful feature to include all such routes 
+
+   resources :articles, only: ["show"]
 end
