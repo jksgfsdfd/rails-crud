362cb8e4a007ae8e13212d55a43229cec3b88d47
maanas <maanas2605@gmail.com>
2022-12-07 14:09:26 +0530

    converts email to lowercase before saving to db..and does case insensitive uniqueness checking for email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :articles
-    validates :username , presence: true , length: {minimum: 3 , maximum:20} , uniqueness: true
+    before_save {self.email = email.downcase}
+    validates :username , presence: true , length: {minimum: 3 , maximum:20} , uniqueness: {case_sensitive: false}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email , presence: true , uniqueness: true , format: {with: VALID_EMAIL_REGEX}
+    validates :email , presence: true , uniqueness: {case_sensitive: false} , format: {with: VALID_EMAIL_REGEX}
 end
